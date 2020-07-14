7d3b6b2335df719d083b028f424659668e3b0d39
mjm681 <62715776+mjm681@users.noreply.github.com>
2020-07-14 22:20:09 +0100

    Update ordinal_pca.R

--- ordinal_pca.R
+++ ordinal_pca.R
@@ -3,27 +3,14 @@
 library(Gifi)
 library(ggplot2)
 
-clara_data <- read.csv("final_data.csv", header = T, check.names = F, na.strings = "NA", row.names = 1)
-
-column_names <- c('caseid', 'ASSESS_SYSBP_VAL', 'ASSESS_CREFILL_NORM', 'ASSESS_GCS_MOTOR', 
-                  'ASSESS_GCS_TOTAL', 'ASSESS_PULSE_VAL', 'ASSESS_RESP_RATE_VAL', 
-                  'INJURY_MECHANISM', 'INJURY_TYPE', 'PATIENT_AGE', 'PATIENT_GENDER', 
-                  'CAPTIME_REVISED_IsMoreThan2', 'Head', 'Face', 'Chest', 'Abdomen', 
-                  'Pelvis', 'Spine', 'Limb', 'Other', 'Has_Lerner_LSI_5_Lerner_time',
-                  'P1_T_or_F')
-
-cols <- c(1, 17, 44, 73, 21, 12, 11, 10, 8, 7, 4, 3, 64:71, 76, 86)
-
-cluster_df <- clara_data[,cols]
-rownames(cluster_df) <- cluster_df$caseid
-cluster_df <- cluster_df[,-1]
-
-cluster_df_na <- cluster_df[complete.cases(cluster_df),]
+cluster_df_na <- read.csv("final_data.csv", header = T, check.names = F, na.strings = "NA", row.names = 1) # Cluster data
 
 ##### Performing PCA #####
 
+# Defining which variables are ordinal
 ordinals = c(T, F, F, T, T, T, T, F, F, T, F, T, T, T, T, T, T, T, T, F, T)
 
+# Perform oPCA
 fit_ord <- princals(cluster_df_na, ndim = 2, ordinal = ordinals, knots = knotsGifi(cluster_df, "E"), verbose = T)
 
 fit_ord_sum <- summary(fit_ord)
@@ -50,17 +37,14 @@ pdf("screeplot.pdf")
 plot(fit_ord, plot.type = "screeplot")
 dev.off()
 
-##### Observations #####
-
-obs_col <- c(17, 44, 73, 21, 12, 11, 10, 8, 7, 4, 3, 64:71, 76, 72, 63, 74, 86)
-
-obs_df <- full_data_na[,obs_col]
+##### Combining Observations and Output Data #####
 
 co_ord <- as.data.frame(fit_ord$objectscores)
 
-cluster_coords <- cbind(obs_df, co_ord)
+cluster_coords <- cbind(cluster_df_na, co_ord)
 
-##### Obs Plots #####
+##### Plots to explore distribution #####
+
 pdf("Combine_Outcome.pdf")
 ggplot(cluster_coords, aes(x = D1, y = D2, colour = Combine_Outcome)) +
   geom_point()
